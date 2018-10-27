05a2161ca93bfa17849129f1e07bc0b529a21c40
dkirton24 <38841728+dkirton24@users.noreply.github.com>
2018-10-27 13:19:04 -0400

    Add files via upload

---
+++ stringTranslator.py
@@ -0,0 +1,5 @@
+data1 = "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj"
+
+for c in data1:
+    data1.replace(c, chr(ord(c)+2))
+print(data1)
