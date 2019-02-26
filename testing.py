ad41c0094b5acc2fd12db7c776210e62e3f98148
dkirton24 <38841728+dkirton24@users.noreply.github.com>
2019-02-26 15:51:58 -0500

    Add files via upload
    Python code that creates to a new text file every 8 digit combination of numbers possible

---
+++ testing.py
@@ -0,0 +1,14 @@
+#!/usr/bin python
+
+nList = open("numberList.txt", "w+")
+n = 00000000
+
+while n < 100000000:
+        if n % 10000 == 0:
+                print("Completed", n, "number of lines so far!")
+        nStr = str(n)
+        nList.write(nStr)
+        nList.write("\n")
+        n += 1
+nList.close()
+print("Done!")
